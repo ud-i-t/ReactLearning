3bef07d5f1057ad20679f5746f64f885e577108c
ud <mizunoawa.web@gmail.com>
2023-07-16 09:41:24 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { title } from 'process';
+
+type Item = {
+  id: number
+  title: string
+}
+
+function Child() {
+  return(
+    <p>hogehoge</p>
+  )
+}
 
 function App() {
+  const items: Item[] = [
+    {
+      id:1,
+      title: "ビャッコーギャモン"
+    },{
+      id:2,
+      title: "Element Zero"
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        { items.map((item: Item) => (<li key={item.id}>{item.title}</li>))}
+      </ul>
+      <Child/>
     </div>
   );
 }
